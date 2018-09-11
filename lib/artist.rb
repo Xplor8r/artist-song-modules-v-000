8a995a6161169b17fec2d7cdc1d340b9dbeaed33
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-11 23:01:05 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,6 +20,17 @@ class Artist
     @@artists
   end
 
+<<<<<<< HEAD
+=======
+  #def self.reset_all
+    #self.all.clear
+  #end
+
+  #def self.count
+    #@@artists.count
+  #end
+
+>>>>>>> 3377475101de7046a438f10ee105b192f7c08fa3
   def add_song(song)
     @songs << song
     song.artist = self
--- lib/song.rb
+++ lib/song.rb
@@ -15,6 +15,21 @@ class Song
     @artist = artist
   end
 
+<<<<<<< HEAD
+=======
+  #def self.reset_all
+    #@@songs.clear
+  #end
+
+  def to_param
+    name.downcase.gsub(' ', '-')
+  end
+
+  #def self.count
+    #self.all.count
+  #end
+
+>>>>>>> 3377475101de7046a438f10ee105b192f7c08fa3
   def self.all
     @@songs
   end
